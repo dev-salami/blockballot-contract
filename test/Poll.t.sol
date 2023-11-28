3fe9f15c1b9ea8067b1fbcf279905e68627e4555
Salami Khalil <salamikhalil02@gmail.com>
2023-11-28 08:35:41 +0100

    Test setup

--- test/Poll.t.sol
+++ test/Poll.t.sol
@@ -7,6 +7,20 @@ import {PollContract} from "../src/Poll.sol";
 
 contract PollTest is Test {
     PollContract pollContract;
+    address constant CREATOR = address(1);
+    address constant USER1 = address(2);
+    address constant USER2 = address(3);
+
+    bool constant PUBLIC_ACCESS = false;
+    address[] WHITELIST = [USER1, USER2];
+    string constant QUESTION = "Who are you ?";
+    string[] OPTIONS = ["Ade", "Bola", "Titi"];
+    modifier MakePoll() {
+        vm.startPrank(CREATOR);
+        pollContract.CreatePoll(PUBLIC_ACCESS, WHITELIST, QUESTION, OPTIONS);
+        vm.stopPrank();
+        _;
+    }
 
     function setUp() public returns (PollContract) {
         pollContract = new DeployPoll().run();
@@ -14,7 +28,21 @@ contract PollTest is Test {
         return pollContract;
     }
 
-    // function testPoll_Id_Initailize() public {
-    //     assertEq(pollContract.POLL_UUID(), 0);
+    function testPoll_Id_Initailize() public {
+        assertEq(pollContract.POLL_UUID(), 0);
+    }
+
+    // function testCreatePoll() public MakePoll {
+    //     assertEq(pollContract.POLL_UUID(), 1);
     // }
+
+    function testPoll_Data_Update() public MakePoll {
+        // assertEq(pollContract.UserPolls()[msg.sender], 0);
+        vm.startPrank(CREATOR);
+        console.log(pollContract.getSinglePoll(0).Creator);
+        assertEq(pollContract.getSinglePoll(0).Creator, CREATOR);
+        assertEq(pollContract.getUserPolls().length, 1);
+        assertEq(pollContract.POLL_UUID(), 1);
+        vm.expectEmit();
+    }
 }
